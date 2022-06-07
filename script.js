406d6dd8eadf888d74e3e8b71fe38866a6da9f96
zbob889 <zroberts889@gmail.com>
2022-06-07 12:42:10 +0200

    finished

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ scissors.addEventListener('click', () => {playRound('scissors')});
 
 const container = document.querySelector('#container');
 
+let playerScore = 0;
+let computerScore = 0;
+let match = 0;
 
 function playRound(playerSelection) {
 
@@ -17,22 +20,71 @@ function playRound(playerSelection) {
         computerSelection =  'Paper'
     } else {
         computerSelection = 'Scissors'
-    }
+    };
 
     if (((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'Scissors')) || ((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'Rock'))|| ((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'Paper'))){
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'Player wins';
+        content.textContent = 'Player wins!';
         container.appendChild(content);
+        playerScore++;
+        match++;
+        const pScore = document.createElement('div');
+        pScore.classList.add('content');
+        pScore.textContent = 'Player score: ' + playerScore;
+        container.appendChild(pScore);
+        const cScore = document.createElement('div');
+        cScore.classList.add('content');
+        cScore.textContent = 'Computer score: ' + computerScore;
+        container.appendChild(cScore);
     } else if (((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'Paper')) || ((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'Scissors'))|| ((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'Rock'))){
         const content = document.createElement('div');
         content.classList.add('content');
         content.textContent = 'Computer wins!';
         container.appendChild(content);
+        computerScore++;
+        match++;
+        const pScore = document.createElement('div');
+        pScore.classList.add('content');
+        pScore.textContent = 'Player score: ' + playerScore;
+        container.appendChild(pScore);
+        const cScore = document.createElement('div');
+        cScore.classList.add('content');
+        cScore.textContent = 'Computer score: ' + computerScore;
+        container.appendChild(cScore);
     } else {
         const content = document.createElement('div');
         content.classList.add('content');
         content.textContent = 'Player draws with the computer!';
         container.appendChild(content);
-    }
-}
+        match++;
+        const pScore = document.createElement('div');
+        pScore.classList.add('content');
+        pScore.textContent = 'Player score: ' + playerScore;
+        container.appendChild(pScore);
+        const cScore = document.createElement('div');
+        cScore.classList.add('content');
+        cScore.textContent = 'Computer score: ' + computerScore;
+        container.appendChild(cScore);
+    };
+
+    if (match === 5) {
+
+        if (playerScore > computerScore){
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = 'Player wins the game!';
+            container.appendChild(content);
+        } else if (computerScore > playerScore){
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = 'Computer wins the game!';
+            container.appendChild(content);
+        } else {
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = 'The game is a draw!';
+            container.appendChild(content);
+        }
+    };
+};
